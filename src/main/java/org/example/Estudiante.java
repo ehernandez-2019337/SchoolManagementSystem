0f780a725ad1ea4571ea48744a653cde2b1021ab
Emerson Hernandez <emersil2005@gmail.com>
2024-09-02 17:56:40 -0600

    Creacion de Pyoyecto y modelos

---
+++ src/main/java/org/example/Estudiante.java
@@ -0,0 +1,60 @@
+package org.example;
+
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String fechaDeNacimiento;
+    private String estado;
+
+    public Estudiante() {
+    }
+
+    public Estudiante(int id, String nombre, String apellido, String fechaDeNacimiento, String estado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.estado = estado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
---
+++ src/main/java/org/example/GestorAcademico.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GestorAcademico {
+    private ArrayList<Estudiante> estudiantes = new ArrayList<>();
+    private ArrayList<Curso> cursos = new ArrayList<>();
+    private HashMap<Integer, ArrayList<Estudiante>> estudiantesPorCurso = new HashMap<>();
+
+    // Métodos para gestionar estudiantes, cursos y las inscripciones
+}
